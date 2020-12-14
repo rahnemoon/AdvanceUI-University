e5e92eb9f579d7beb8346713b3e7631082cb2cb8
asalarp <56116441+asalarp@users.noreply.github.com>
2020-12-14 22:38:42 +0100

    Update: get input from vue

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, url_for, json
+from flask import Flask, jsonify, render_template, json, request
 from flask_cors import CORS
 from google.cloud import texttospeech
 import os
@@ -17,33 +17,24 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./emoty-tts-key.json"
 
 # sanity check route
-def text_to_speech():
+def text_to_speech(message):
     client = texttospeech.TextToSpeechClient()
-    synthesis_input = texttospeech.SynthesisInput(text="hello erfan, how are you doing, i hope that i can satisfy your needs, come on man let's do it, i'm hornny")
+    synthesis_input = texttospeech.SynthesisInput(text=message)
     voice = texttospeech.VoiceSelectionParams(
         language_code="en-US", name="en-US-Wavenet-H", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
     )
-
-    # Select the type of audio file you want returned
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.LINEAR16
     )
     response = client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
-
-    # The response's audio_content is binary.
     with open("Cache/output.wav", "wb") as out:
-        # Write the response to the output file.
         out.write(response.audio_content)
         print('Audio content written to file "output.wav"')
-    
-    return jsonify("test")
-    
 
-@app.route('/', methods=['GET'])
+    
 def lip_sync():
-    text_to_speech()
     cmd = ["./Rhubarb_Lip_Sync/rhubarb","-o", "Cache/output.json", "Cache/output.wav", "-f", "json"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
@@ -53,7 +44,20 @@ def lip_sync():
     with open("Cache/output.json", "r") as result:
         data = json.load(result)
 
-    return jsonify(data)
+
+@app.route('/text-input', methods=['POST'])
+def text_input():
+    response_obj = {"status": "successful"}
+    if request.method == "POST":
+        post_data = request.get_json()
+        #response_obj["message"] = post_data["txt"]
+        text_to_speech(post_data["txt"])
+        lip_sync()
+
+    else:
+        response_obj["status"] = "failed"
+
+    return jsonify(response_obj)
 
 if __name__ == '__main__':
     app.run()
